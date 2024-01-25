79e3bf578718843d54e011bdb2864f8f912c0d78
nagvanshi9275 <106686974+nagvanshi9275@users.noreply.github.com>
2024-01-25 18:39:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './style.css';
+
+import Main from "./Main"
+
+import Login from "./Main";
+
+export default function App() {
+  return (
+    <div>
+      
+
+       <Main/>
+
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Main.js
@@ -0,0 +1,59 @@
+
+
+
+import React from "react";
+
+import "./style.css";
+
+export default function Main(){
+
+      return(
+
+
+        <div className="box">
+
+
+          
+
+         <input className="text" text="text"/>
+
+         <input className="text" text="text"/>
+
+           <button>create account</button>
+          
+           <button>signin google</button>
+
+
+        </div>
+
+
+
+
+
+      )
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
